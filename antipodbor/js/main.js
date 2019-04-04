0d58361d00a81ff6ce14466c2965ef1c59cf64f2
Sergey Margelov <smargelov@gmail.com>
2019-04-05 00:29:31 +0300

    Добавил две страницы сайта анттподбор

---
+++ antipodbor/js/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".burger-top").click(function(){$(this).toggleClass("burger-top--active"),$(".nav-top").toggleClass("nav-top--mobile")}),$(".sidebar__arrow").click(function(){$(".icon-arrow").hasClass("close")?($(".icon-arrow").removeClass("close"),$(".sidebar").removeClass("mobile")):($(".icon-arrow").addClass("close"),$(".sidebar").addClass("mobile"))})});
+//# sourceMappingURL=main.js.map
