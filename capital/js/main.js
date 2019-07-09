76a7e165e8cd268bc80fdc4d7dd7b0a8e6bafbf0
Sergey Margelov <smargelov@gmail.com>
2019-07-09 14:37:58 +0300

    Адаптив страницы новостей

--- capital/js/main.js
+++ capital/js/main.js
@@ -1,2 +1,2 @@
-
+$(document).ready(function(){$(".header__burger").click(function(){var e=this;$(e).hasClass("header__burger--active")?($(e).removeClass("header__burger--active"),$(".header__bottomline").slideUp()):($(e).addClass("header__burger--active"),$(".header__bottomline").slideDown())})});
 //# sourceMappingURL=main.js.map
